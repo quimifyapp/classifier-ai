caef9cb6d4885ffaa724f7c907f23252152a22e1
Pablo Asenjo Navas-Parejo <pablo@quimify.com>
2023-04-26 19:45:33 +0200

    Create API methods for testing purposes

--- api/api.py
+++ api/api.py
@@ -8,9 +8,24 @@ app = Flask(__name__)
 model = Model()
 
 
-@app.route('/formula-name/<text>')
+@app.route('/formula-or-name/<text>')
 def formula_or_name(text):
-    return str(model.predict(text))
+    return str(model.formula_or_name(text))
+
+
+@app.route('/formula-inorganic-or-organic/<text>')
+def formula_inorganic_or_organic(text):
+    return str(model.formula_inorganic_or_organic(text))
+
+
+@app.route('/name-inorganic-or-organic/<text>')
+def name_inorganic_or_organic(text):
+    return str(model.name_inorganic_or_organic(text))
+
+
+@app.route('/classify/<text>')
+def classify(text):
+    return str(model.classify(text))
 
 
 if __name__ == '__main__':
