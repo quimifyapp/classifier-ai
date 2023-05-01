e8d0dbee833b45bc8af73a526b4cd8d922bfdca4
Pablo Asenjo Navas-Parejo <pablo@quimify.com>
2023-05-01 16:04:06 +0200

    Make model character-based by turning text to [m o n o s p a c e] in data_standardization

--- src/model.py
+++ src/model.py
@@ -1,16 +1,36 @@
 import tensorflow as tf
-
-
-@tf.keras.utils.register_keras_serializable()
-def data_standardization(input_data):  # CH3-CH=CH-CH(NO2)Br
-    input_data = tf.strings.lower(input_data)  # ch3-ch=ch-ch(no2)br
-    input_data = tf.strings.regex_replace(input_data, "[^a-zà-ú]", ' ')  # ch  ch ch ch no  br # TODO reconsider
-    return tf.strings.regex_replace(input_data, "\s+", ' ')  # ch ch ch ch no br
+from tensorflow.python.keras import layers, losses
 
 
 class Model:
-    def __init__(self, model_path):
-        self.model = tf.keras.models.load_model(model_path)
+
+    @staticmethod
+    @tf.keras.utils.register_keras_serializable()
+    def data_standardization(input_data):  # "CH3-CH=CH-CH(NO2)Br"
+        input_data = tf.strings.lower(input_data)  # "ch3-ch=ch-ch(no2)br"
+        return tf.strings.regex_replace(input_data, r".", r"\0 ")  # "c h 3 - c h = c h - c h ( n o 2 ) b r"
+
+    def __init__(self, model_path=None, max_features=None, embedding_dim=None):
+        if model_path is not None:
+            self.model = tf.keras.models.load_model(model_path)
+        elif max_features is not None and embedding_dim is not None:
+            self.model = tf.keras.Sequential([
+                layers.Embedding(max_features + 1, embedding_dim),
+                layers.Dropout(0.2),
+                layers.Dense(256, activation="relu"),
+                layers.GlobalAveragePooling1D(),
+                layers.Dropout(0.2),
+                layers.Dense(1),
+            ])
+
+            self.model.compile(
+                optimizer="adam",
+                metrics=tf.metrics.BinaryAccuracy(threshold=0.0),
+                loss=losses.BinaryCrossentropy(from_logits=True),
+            )
 
     def predict(self, text):
         return self.model.predict([text])[0][0]
+
+    def get(self):
+        return self.model
