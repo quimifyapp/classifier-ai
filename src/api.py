584be72faa7f13ea02b0ee1490d1654f6067777c
Pablo Asenjo Navas-Parejo <pablo@quimify.com>
2024-03-04 20:00:53 +0100

    Improve logging in production even more

--- src/api.py
+++ src/api.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 # Constants:
 
-EMPTY = ""
+UNKNOWN = ""
 
 INORGANIC_FORMULA = "inorganicFormula"
 ORGANIC_FORMULA = "organicFormula"
@@ -47,33 +47,40 @@ def is_organic_name(score):
     return score > 0.5
 
 
-def with_log(text, result):
-    logger.info("\"" + text + "\" -> \"" + result + "\"")
+def with_log(input_text, result):
+    label = "unknown" if result == UNKNOWN else result
+
+    red = "\033[91m"
+    green = "\033[92m"
+    color = red if result == UNKNOWN else green
+
+    logger.info("{:<25} <- {}".format(color + label + "\033[0m", '"' + input_text + '"'))
+
     return result
 
 
 @app.route("/")
 def classify():
-    text = request.args.get('input')
+    input_text = request.args.get('input')
 
-    score = formula_name_model.predict(text)
+    score = formula_name_model.predict(input_text)
 
     if is_formula(score):
-        formula_score = formula_inorganic_organic_model.predict(text)
+        formula_score = formula_inorganic_organic_model.predict(input_text)
 
         if is_inorganic_formula(formula_score):
-            return with_log(text, INORGANIC_FORMULA)
+            return with_log(input_text, INORGANIC_FORMULA)
         elif is_organic_formula(formula_score):
-            return with_log(text, ORGANIC_FORMULA)
+            return with_log(input_text, ORGANIC_FORMULA)
     elif is_name(score):
-        name_score = name_inorganic_organic_model.predict(text)
+        name_score = name_inorganic_organic_model.predict(input_text)
 
         if is_inorganic_name(name_score):
-            return with_log(text, INORGANIC_NAME)
+            return with_log(input_text, INORGANIC_NAME)
         elif is_organic_name(name_score):
-            return with_log(text, ORGANIC_NAME)
+            return with_log(input_text, ORGANIC_NAME)
 
-    return with_log(text, EMPTY)
+    return with_log(input_text, UNKNOWN)
 
 
 # Run at startup:
